a7036a71b2eafc8f7eb06a908603f3c4bd518209
MarioJardonS <mojodanti@hotmail.com>
2026-06-06 13:28:23 -0600

    core interseccion

--- Scripts/core_network.py
+++ Scripts/core_network.py
@@ -17,24 +17,40 @@ def edges(red):
 	return aristas
 
 
-core_network = edges(redes[0])
+all_edges = edges(redes[0])
+conteo = {}
+
+for e in all_edges:
+	conteo[e] = 1
+
+
 
 for j in range(1,len(redes)):
-	aristas_nuevas = edges(redes[j])
-	if len(core_network.intersection(aristas_nuevas)) > 0:
-		core_network = core_network.intersection(aristas_nuevas)
-	else:
-		print(j)
-		break 
+	aristas = edges(redes[j])
+	aristas_nuevas = aristas.difference(all_edges)
+	aristas = aristas.intersection(all_edges)
+	for e in aristas_nuevas:
+		conteo[e] = 1
+	for e in aristas:
+		conteo[e] = conteo[e] + 1
+	all_edges = all_edges.union(aristas_nuevas)
 
+#	if len(core_network.intersection(aristas_nuevas)) > 0:
+#		core_network = core_network.intersection(aristas_nuevas)
+#	else:
+#		print(j)
+#		break
+	
 
 file = open(f"../Networks/{output}.tsv" , "w")
+file2 = open(f"../Networks/edges.tsv" , "w")
 
-for edge in core_network:
-	file.write(f"{edge[0]}\t {edge[1]} \n")
+for edge in conteo:
+	file.write(f"{edge[0]}\t{edge[1]}\t{conteo[edge]}\n")
+	file2.write(f"{edge[0]}\t{edge[1]}\n")
 
 file.close()
-
+file2.close()
 
 
 
