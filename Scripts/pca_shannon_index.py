4af6418d7c03a56b0afb31819f038db221f399a5
MarioJardonS <mojodanti@hotmail.com>
2026-06-17 17:09:05 -0600

    pca shannon

---
+++ Scripts/pca_shannon_index.py
@@ -0,0 +1,78 @@
+# importing required libraries
+# Tratamiento de datos
+# ==============================================================================
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+
+# Gráficos
+# ==============================================================================
+import matplotlib.pyplot as plt
+import matplotlib.font_manager
+from matplotlib import style
+style.use('ggplot') or plt.style.use('ggplot')
+
+# Preprocesado y modelado
+# ==============================================================================
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import scale
+
+# Configuración warnings
+# ==============================================================================
+import warnings
+warnings.filterwarnings('ignore')
+
+
+dataset = pd.read_table("pathway_shannon_index.tsv" )
+dataset = dataset.transpose()
+dataset = dataset.iloc[ : , 1: ] 
+
+
+
+scaler = StandardScaler()
+
+X = scaler.fit_transform(dataset)
+
+PCA = PCA(n_components=2)
+components = PCA.fit_transform(X)
+PCA.components_
+
+metadata = pd.read_csv("../Data/metadata.csv" )
+file = open("../Data/argumentos.txt")
+
+Y = []
+Z = []
+
+def cl(diag):
+	if diag == "Healthy":
+		return 'b'
+	if diag == "UC":
+		return 'g'
+	if diag == "CD":
+		return 'r'
+	if diag == "Obese":
+		return 'c'
+
+for line in file:
+	muestra = line[0:(len(line)-1)]
+	for i in range(metadata.shape[0]):
+		if metadata.loc[ i , "SampleID" ] == muestra:
+			c = cl(metadata.loc[i , "Diagnosis"]) 
+			Y.append(c)
+			Z.append(metadata.loc[i , "Diagnosis"])
+
+
+Z = pd.Series(Z)
+
+
+componentsDf = pd.DataFrame(data = components, columns = ['PC1', 'PC2'])
+pcaDf = pd.concat([componentsDf, Z], axis=1)
+
+plt.figure(figsize=(12, 6))
+plt.scatter(data=pcaDf, x="PC1", y="PC2" , color = Y)
+plt.savefig("pca_vertices_shannon.png")
+#sns.scatterplot(data=pcaDf, x="PC1", y="PC2")
+
+#salida.to_tsv("compo.tsv" , sep = "\t")
